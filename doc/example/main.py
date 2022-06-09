795caaf036caace24e74fe26814a4e3dd16b43d9
Geoff Low <glow@mdsol.com>
2022-06-09 11:03:49 +0100

    Pass the subject ID

--- doc/example/main.py
+++ doc/example/main.py
@@ -1,3 +1,5 @@
+import sys
+
 from windows import StudyWindow
 
 BASEURL = "https://api.logicahealth.org/soaconnectathon30/open"
@@ -8,10 +10,12 @@ def run(subject_id, study_id="H2Q-MC-LZZT"):
     protocol = window.get_protocol()
     processed = window.process_protocol(protocol)
     window.get_subject_scheme(subject_id, processed)
-    
 
 
 if __name__ == '__main__':
     STUDY_ID = "H2Q-MC-LZZT"
-    SUBJECT_ID = "01-701-1047"
+    if len(sys.argv) > 1:
+        SUBJECT_ID = sys.argv[1]
+    else:
+        SUBJECT_ID = "01-701-1047"
     run(SUBJECT_ID, STUDY_ID)
