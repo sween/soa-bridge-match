d1850f86aa5eb31a713962455c430cde922ff14e
Ron Sweeney <ron.sweeney@integrationrequired.com>
2022-09-07 18:15:29 +0000

    send fundles

---
+++ upstream/post_fhir_bundle.sh
@@ -0,0 +1,3 @@
+#API_KEY=hqj0mmP3ky9cbJa8GhMdx3nsa3hzXc7428RUVpe6
+
+curl -v -sbi -X POST -H "Content-Type: application/json"  -H "x-api-key:hqj0mmP3ky9cbJa8GhMdx3nsa3hzXc7428RUVpe6" -d @LZZT_FHIR_Bundle_10_Patients_All_Resources_dupes.json https://fhir.6eyq21exgcnv.workload-nonprod-fhiraas.isccloud.io
